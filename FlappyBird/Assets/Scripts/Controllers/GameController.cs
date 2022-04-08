0319e931e1d6fa19f14c3914dcf538ea12b566c4
Himadri Roy <himadri.roy@orchids.edu.in>
2022-04-08 12:05:05 +0530

    unity

---
+++ FlappyBird/Assets/Scripts/Controllers/GameController.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FlappyBird.Controller
+{
+    public class GameController : MonoBehaviour
+    {
+        [SerializeField] GameObject[] WallArray;
+
+        private void initObstacleWalls()
+        {
+
+        }
+    }
+}
+
---
+++ FlappyBird/Assets/Scripts/Views/WallView.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallView : MonoBehaviour
+{
+    // Start is called before the first frame update
+    //private void 
+}
